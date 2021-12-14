cae37c8f5efae8b4db54a2820272d5f4a50a6aa0
Tropheus Jay <jverrellijr@icloud.com>
2021-12-13 19:10:20 -0500

    supplier shenanigans

--- src/main/java/com/tterrag/registrate/util/nullness/NonNullSupplier.java
+++ src/main/java/com/tterrag/registrate/util/nullness/NonNullSupplier.java
@@ -5,9 +5,6 @@ import java.util.function.Supplier;
 
 @FunctionalInterface
 public interface NonNullSupplier<@NonnullType T> extends Supplier<T> {
-    
-    @Override
-    T get();
 
     static <T> NonNullSupplier<T> of(Supplier<@NullableType T> sup) {
         return of(sup, () -> "Unexpected null value from supplier");
